0d791e2d4d4b0c03ed4e74992f0afcfce4018aad
nicaodev <alexandre.nicos@gmail.com>
2022-12-03 19:21:50 -0300

    interface, mock

--- todo-app/src/app/modules/home/components/todo-list/todo-list.component.ts
+++ todo-app/src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ITaskList } from '../../model/itask-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
 
+  public taskList: Array<ITaskList> =
+    [
+      { task: "Nova", checked: true },
+      { task: "Nova_2", checked: false }
+    ];
+
+  constructor() {
+
+  }
+  ngOnInit(): void {
+  }
 }
